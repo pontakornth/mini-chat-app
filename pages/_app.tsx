32f6cdde2951692fde2afb51a745c5a372e689ef
pontakornth <pontakorn61@gmail.com>
2021-04-09 14:37:01 +0700

    chore: setup provider test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,5 +1,10 @@
 import Header from '../components/Header'
-import { render, screen } from '@testing-library/react'
+import { render as rawRender, screen } from '@testing-library/react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const render = (ui, options?: Parameters<typeof rawRender>[1]): ReturnType<typeof rawRender> => {
+  return rawRender(<ChakraProvider>{ui}</ChakraProvider>, options)
+}
 
 it('render correctly', () => {
   render(<Header />)
