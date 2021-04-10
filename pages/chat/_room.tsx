e40d4b09b13d58df5714eba2b7fcc99ca8b13090
pontakornth <pontakorn61@gmail.com>
2021-04-10 11:20:10 +0700

    feat: Make chat room

---
+++ pages/chat/_room.tsx
@@ -0,0 +1,13 @@
+import { Container, Flex, Heading } from '@chakra-ui/layout'
+import { NextPage } from 'next'
+
+const ChatRoom: NextPage = () => {
+  return (
+    <Container>
+      <Heading>Room</Heading>
+      <Flex></Flex>
+    </Container>
+  )
+}
+
+export default ChatRoom
