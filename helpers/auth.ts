d10b9ff1559486c5429e4f9c6d74210ef3b4a808
pontakornth <pontakorn61@gmail.com>
2021-04-10 11:19:48 +0700

    feat: Add blank authentication

---
+++ helpers/auth.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function loginWithEmail(email: string, password: string): Promise<void> {
+  // TODO: Make the authentication real
+  throw new Error('Not implemented')
+}
